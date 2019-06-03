eb9fcb2eb0bae9d20caf6ec8e64a738d1e491c6b
Ewa Fengler <ewa.fen@interia.pl>
2019-06-03 19:15:58 +0200

    sort insances in all instances

--- instances_script.py
+++ instances_script.py
@@ -8,12 +8,19 @@ instances_filenames = [path.join(INSTANCES_PATH, f) for f in listdir(INSTANCES_P
 result_filename = path.join(BASE_DIR_PATH, "all_instances.csv")
 
 
+keys = [i.split(" ") for i in instances_filenames]
+keys = [int(k[0][k[0].rfind('/') + 1:] + k[1].zfill(2) + k[2] + k[3][:-4].zfill(2)) for k in keys]
+after_sort = list(zip(*sorted(zip(keys,instances_filenames), key = lambda t: t[0])))[1]
+
 with open(result_filename, 'w') as result_file:    
-    for filename in instances_filenames:
+    for filename in after_sort:
         with open(filename, 'r') as file:
             a, s, d, f = filename.split(" ")
             a = a[a.rfind('/') + 1:]
             f = f[:-4]
-            result_file.write("Rozmiar macierzy: " + a + ", Rozmiar bloku: " + s + ", Liczba obliczanych macierzy: " + f + ", Wersja kodu: " + d + ",,,,,,,,,,,,,,,,,,,,,,,,,\n")
+#             print("Rozmiar macierzy: " + a + ", Rozmiar bloku: " + s + ", Liczba obliczanych macierzy: " + f + ", Wersja kodu: " + d + ",,,,,,,,,,,,,,,,,,,,,,,,,,,\n")
+            result_file.write("Rozmiar macierzy: " + a + ", Rozmiar bloku: " + s + ", Liczba obliczanych macierzy: " + f + ", Wersja kodu: " + d + ",,,,,,,,,,,,,,,,,,,,,,,,,,,\n")
             result_file.write(file.read())
-            result_file.write("\n\n")
+            result_file.write(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n")
+            result_file.write(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n")            
+            
